a521855821a17a4ef2967f797317c5efd0b79668
Ghaieth Grami <ghaieth.grami06@gmail.com>
2024-04-27 15:37:58 +0200

    add new project

--- src/data/projects.js
+++ src/data/projects.js
@@ -9,25 +9,27 @@ export const projectsData = [
   {
     id: 1,
     title: 'Pokedex-App with PokeAPI',
-    category: 'React   TailwindCSS  Vite',
+    category: 'React  -  TailwindCSS  -  Vite',
     img: Project1,
     sourceCode: 'https://github.com/Ghaieth1/Pokemon-app',
     livePreview: 'https://pokemon-pokedex1.netlify.app',
   },
+
   {
     id: 2,
-    title: 'Quizz App with Trivia API',
-    category: 'React   TailwindCSS   Vite',
+    title: 'Fast Food Ordering App',
+    category: 'React -  bootstrap  ',
+    img: UIImage1,
     sourceCode: 'https://github.com/Ghaieth1/Quizzical-App',
-    livePreview: 'https://quizzical-app1.netlify.app',
-
-    img: Project2,
+    livePreview: 'https://food-react-app-six.vercel.app/',
   },
   {
     id: 3,
-    title: 'Project Management UI',
-    category: 'UI/UX Design',
-    img: UIImage1,
+    title: 'Quizz App with Trivia API',
+    category: 'React  -  TailwindCSS  -  Vite',
+    img: Project2,
+    sourceCode: 'https://github.com/Ghaieth1/Food-react-app/tree/main',
+    livePreview: 'https://quizzical-app1.netlify.app',
   },
   {
     id: 4,
