7ff5d734f6e648835a9bfe526cb1b867f51a4e78
Ghaieth Grami <ghaieth.grami06@gmail.com>
2024-05-03 12:36:24 +0200

    projects update

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,39 +2,39 @@
 
 import UIImage1 from '../images/ui-project-1.jpg';
 import UIImage2 from '../images/ui-project-2.jpg';
-import Project1 from '../images/pokedex.jpg';
-import Project2 from '../images/Quizzical.jpg';
+import Project2 from '../images/pokedex.png';
+import Project1 from '../images/Food App.png';
+import Project3 from '../images/Cv Builder.png';
 
 export const projectsData = [
   {
     id: 1,
-    title: 'Pokedex-App with PokeAPI',
-    category: 'React  -  TailwindCSS  -  Vite',
-    img: Project1,
-    sourceCode: 'https://github.com/Ghaieth1/Pokemon-app',
-    livePreview: 'https://pokemon-pokedex1.netlify.app',
-  },
-
-  {
-    id: 2,
     title: 'Fast Food Ordering App',
-    category: 'React -  bootstrap  ',
-    img: UIImage1,
-    sourceCode: 'https://github.com/Ghaieth1/Quizzical-App',
+    category: 'React -  bootstrap - Firebase',
+    img: Project1,
+    sourceCode: 'https://github.com/Ghaieth1/Food-react-app',
     livePreview: 'https://food-react-app-six.vercel.app/',
   },
   {
-    id: 3,
-    title: 'Quizz App with Trivia API',
+    id: 2,
+    title: 'Pokedex-App with PokeAPI',
     category: 'React  -  TailwindCSS  -  Vite',
     img: Project2,
-    sourceCode: 'https://github.com/Ghaieth1/Food-react-app/tree/main',
-    livePreview: 'https://quizzical-app1.netlify.app',
+    sourceCode: 'https://github.com/Ghaieth1/Pokemon-app',
+    livePreview: 'https://pokemon-pokedex1.netlify.app',
+  },
+  {
+    id: 3,
+    title: 'Cv Builder App',
+    category: 'React  -  TailwindCSS  -  Vite',
+    img: Project3,
+    sourceCode: 'https://github.com/Ghaieth1/Cv-Builder',
+    livePreview: 'https://cv-builder1.netlify.app',
   },
   {
     id: 4,
-    title: 'Cloud Storage Platform',
-    category: 'UI/UX Design',
+    title: 'Mobile App (Coming Soon)',
+    category: 'React Native ',
     img: UIImage2,
   },
 ];
