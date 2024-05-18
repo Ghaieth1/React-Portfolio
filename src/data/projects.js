c3b02387f3527243dd2ad7d95d85895e940f11af
Ghaieth Grami <ghaieth.grami06@gmail.com>
2024-05-18 11:21:29 +0200

    new project

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,32 +2,41 @@
 
 import UIImage1 from '../images/ui-project-1.jpg';
 import UIImage2 from '../images/ui-project-2.jpg';
-import Project2 from '../images/pokedex.png';
-import Project1 from '../images/Food App.png';
-import Project3 from '../images/Cv Builder.png';
+import Project3 from '../images/pokedex.png';
+import Project2 from '../images/Food App.png';
+import Project4 from '../images/Cv Builder.png';
+import Project1 from '../images/Chat App.png';
 
 export const projectsData = [
   {
     id: 1,
     title: 'Fast Food Ordering App',
-    category: 'React -  bootstrap - Firebase',
+    category: 'React -  Zustand - Firebase',
     img: Project1,
+    sourceCode: 'https://github.com/Ghaieth1/Chat-App-React',
+    livePreview: 'https://chat-app-react-eosin.vercel.app/',
+  },
+  {
+    id: 2,
+    title: 'Fast Food Ordering App',
+    category: 'React -  bootstrap - Firebase',
+    img: Project2,
     sourceCode: 'https://github.com/Ghaieth1/Food-react-app',
     livePreview: 'https://food-react-app-six.vercel.app/',
   },
   {
-    id: 2,
+    id: 3,
     title: 'Pokedex-App with PokeAPI',
     category: 'React  -  TailwindCSS  -  Vite',
-    img: Project2,
+    img: Project3,
     sourceCode: 'https://github.com/Ghaieth1/Pokemon-app',
     livePreview: 'https://pokemon-pokedex1.netlify.app',
   },
   {
-    id: 3,
+    id: 4,
     title: 'Cv Builder App',
     category: 'React  -  TailwindCSS  -  Vite',
-    img: Project3,
+    img: Project4,
     sourceCode: 'https://github.com/Ghaieth1/Cv-Builder',
     livePreview: 'https://cv-builder1.netlify.app',
   },
